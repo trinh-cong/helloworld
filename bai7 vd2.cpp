9ef50c0ad8a64edbe4ca58edcf6c2c4522ebae3b
trinh-cong <111448704+trinh-cong@users.noreply.github.com>
2022-08-24 16:36:05 +0700

    dd
    dd

--- bai7 vd2.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(int argc, char*argv []){
-int year;
-printf("\nPlease enter a year :");
-scanf("%d",&year);
-if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
-printf("\n%d is a leap year", year); 
-return 0;
-}
